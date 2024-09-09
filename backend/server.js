ccaec8c335597fb96d9f9414e48b89e77e02c8b8
haywood-d-johnson <haywood.d.johnson@gmail.com>
2024-09-09 17:34:32 -0500

    init: heroku init

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 require("./routes/student.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
